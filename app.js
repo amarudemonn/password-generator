f60b94dae6504cd79593fe8707f53b9c7ccc141f
amarudemonn <amarudemonn@gmail.com>
2019-10-26 16:20:42 +0300

    UX is done

--- app.js
+++ app.js
@@ -8,6 +8,80 @@ const asterixSignSwitch = document.querySelector('#asterix-sign');
 
 const password = new Password();
 
-numbersSwitch.addEventListener('change', () => {
-  console.log('IT WORKS!')
+let isNumbersSwitchChanged = false;
+let isLowerCaseSwitchChanged = false;
+let isUpperCaseSwitchChanged = false;
+let isDollarSignSwitchChanged = false;
+let isUnderscoreSignSwitchChanged = false;
+let isAmpersandSignSwitchChanged = false;
+let isAsterixSignSwitchChanged = false;
+
+numbersSwitch.addEventListener('change', e => {
+  if (!isNumbersSwitchChanged) {
+    isNumbersSwitchChanged = true;
+    password.addNumbers();
+  } else {
+    isNumbersSwitchChanged = false;
+    password.deleteNumbers();
+  }
+});
+
+lowerCaseSwitch.addEventListener('change', e => {
+  if (!isLowerCaseSwitchChanged) {
+    isLowerCaseSwitchChanged = true;
+    password.addLowerCaseCharacters();
+  } else {
+    isLowerCaseSwitchChanged = false;
+    password.deleteLowerCaseCharacters();
+  }
+});
+
+upperCaseSwitch.addEventListener('change', e => {
+  if (!isUpperCaseSwitchChanged) {
+    isUpperCaseSwitchChanged = true;
+    password.addUpperCaseCharacters();
+  } else {
+    isUpperCaseSwitchChanged = false;
+    password.deleteUpperCaseCharacters();
+  }
+});
+
+dollarSignSwitch.addEventListener('change', e => {
+  if (!isDollarSignSwitchChanged) {
+    isDollarSignSwitchChanged = true;
+    password.addDollarSign();
+  } else {
+    isDollarSignSwitchChanged = false;
+    password.deleteDollarSign();
+  }
+});
+
+underscoreSignSwitch.addEventListener('change', e => {
+  if (!isUnderscoreSignSwitchChanged) {
+    isUnderscoreSignSwitchChanged = true;
+    password.addUnderscoreSign();
+  } else {
+    isUnderscoreSignSwitchChanged = false;
+    password.deleteUnderscoreSign();
+  }
+});
+
+ampersandSignSwitch.addEventListener('change', e => {
+  if (!isAmpersandSignSwitchChanged) {
+    isAmpersandSignSwitchChanged = true;
+    password.addAmpersandSign();
+  } else {
+    isAmpersandSignSwitchChanged = false;
+    password.deleteAmpersandSign();
+  }
+});
+
+asterixSignSwitch.addEventListener('change', e => {
+  if (!isAsterixSignSwitchChanged) {
+    isAsterixSignSwitchChanged = true;
+    password.addAsterixSign();
+  } else {
+    isAsterixSignSwitchChanged = false;
+    password.deleteAsterixSign();
+  }
 });
